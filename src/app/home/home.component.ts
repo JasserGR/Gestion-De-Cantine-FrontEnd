2b267a4ba6e041139a861f96e42b9e570672f6c1
Aziz Sandid <sandid.aziz123@gmail.com>
2025-01-11 20:57:13 +0100

    add service dish & daily menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
+import { Dish } from '../models/dish.type';
+import { catchError } from 'rxjs';
+import { DishCardComponent } from '../dish-card/dish-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DishCardComponent , CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+      dishService=inject(DishService);
+      dailyDish: Dish[] = [];
+    
+      ngOnInit(): void {
+        this.dishService.getDailyMenu()
+        .pipe(
+          catchError((error) => {
+            console.error('Error fetching Daily Menu:', error);
+            throw error;
+          }
+        ))
+        .subscribe((data) => {
+          data.map((dish) => {
+            this.dailyDish.push(dish);
+        });
+      })
+ }
+  
 }
